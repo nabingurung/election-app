c90fb6e3e42b94f8b4390faf891b3ab3add01de8
nabin grg <ngurung@rekor.ai>
2025-01-11 10:40:41 -0500

    Refactor environment configuration to use import.meta.env for AWS settings

--- src/election-app/src/environments/environment.development.ts
+++ src/election-app/src/environments/environment.development.ts
@@ -1,9 +1,9 @@
-// environment.dev.ts
 export const environment = {
-    production: true,
-    awsConfig: {
-      region: process.env["AWS_REGION"] || '',
-      userPoolId: process.env["AWS_USER_POOL_ID"] || '',
-      userPoolWebClientId: process.env["AWS_USER_POOL_WEB_CLIENT_ID"] || '',
-    },
-  };
+  production: false,
+  awsConfig: {
+    region: import.meta.env.NG_APP_AWS_REGION,
+    userPoolId: import.meta.env.NG_APP_AWS_USER_POOL_ID,
+    userPoolWebClientId: import.meta.env.NG_APP_AWS_USER_POOL_WEB_CLIENT_ID,
+  },
+};
+
--- src/election-app/src/environments/environment.ts
+++ src/election-app/src/environments/environment.ts
@@ -1,13 +1,9 @@
-import { config } from 'dotenv';
-
-config(); // Load variables from .env
-
 export const environment = {
   production: false,
   awsConfig: {
-    region: process.env['AWS_REGION'] || 'default-region',
-    userPoolId: process.env['AWS_USER_POOL_ID'] || 'default-pool-id',
-    userPoolWebClientId: process.env['AWS_USER_POOL_WEB_CLIENT_ID'] || 'default-client-id',
+    region: import.meta.env.NG_APP_AWS_REGION,
+    userPoolId: import.meta.env.NG_APP_AWS_USER_POOL_ID,
+    userPoolWebClientId: import.meta.env.NG_APP_AWS_USER_POOL_WEB_CLIENT_ID,
   },
 };
 
